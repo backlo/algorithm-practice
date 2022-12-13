b4bb31b90cc0e6b538092006c3f90afe1afc99b4
backlo <hjun4512@gmail.com>
2022-12-13 15:20:30 +0900

    프로그레머스 12925번 문제 풀기

---
+++ src/programmers/a_1_level/Solution12925.java
@@ -0,0 +1,16 @@
+package programmers.a_1_level;
+
+public class Solution12925 {
+
+    public static int solution(String s) {
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+        String s = "1234";
+        System.out.println(solution(s));
+
+        String s2 = "-12345";
+        System.out.println(solution(s2));
+    }
+}
