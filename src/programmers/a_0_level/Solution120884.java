49b0c5221075810ba534cdf138f15b8fd8673418
backlo <hjun4512@gmail.com>
2023-01-04 14:23:06 +0900

    프로그레머스 120884 문제 풀기

---
+++ src/programmers/a_0_level/Solution120884.java
@@ -0,0 +1,23 @@
+package programmers.a_0_level;
+
+public class Solution120884 {
+
+    public static int solution(int chicken) {
+        int answer = 0;
+
+        while (chicken >= 10) {
+            answer += chicken / 10;
+            chicken = chicken / 10 + chicken % 10;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int chicken = 100;
+        System.out.println(solution(chicken));
+
+        int chicken2 = 1081;
+        System.out.println(solution(chicken2));
+    }
+}
