a6952425af3666be6c07e49b631767bc94c0e426
backlo <hjun4512@gmail.com>
2023-01-26 16:38:22 +0900

    프로그레머스 12951 문제 풀기

--- src/programmers/a_2_level/Solution12951.java
+++ src/programmers/a_2_level/Solution12951.java
@@ -1,2 +1,30 @@
-package programmers.a_2_level;public class Solution12951 {
+package programmers.a_2_level;
+
+public class Solution12951 {
+
+    public static String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        String[] words = s.split(" ", -1);
+
+        for (String word : words) {
+            if (word.isEmpty()) {
+                answer.append(" ");
+                continue;
+            }
+
+            answer.append(word.substring(0, 1).toUpperCase())
+                    .append(word.substring(1).toLowerCase())
+                    .append(" ");
+        }
+
+        return answer.substring(0, answer.toString().length() - 1);
+    }
+
+    public static void main(String[] args) {
+        String s = "  3people  unFollowed  me  ";
+        System.out.println(solution(s));
+
+        String s2 = "for the last week";
+        System.out.println(solution(s2));
+    }
 }
