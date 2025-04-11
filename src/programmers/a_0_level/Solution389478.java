3e57077aa914f254a78ffcf14da5ec51c30a87e9
backlo <hjun4512@gmail.com>
2025-04-11 17:42:11 +0900

    resolve 389478

--- src/programmers/a_0_level/Solution389478.java
+++ src/programmers/a_0_level/Solution389478.java
@@ -3,6 +3,21 @@ package programmers.a_0_level;
 public class Solution389478 {
 
     public static void main(String[] args) {
+        Solution389478 solution = new Solution389478();
+        System.out.println(solution.solution(22, 6, 8));
+        System.out.println(solution.solution(13, 3, 6));
+    }
 
+    public int solution(int n, int w, int num) {
+        int answer = 0;
+        int row = (num - 1) / w;
+        int col = (row % 2 == 0) ? (num - 1) % w : (w - 1 - (num - 1) % w);
+
+        for (; row < (n + w - 1) / w; row++) {
+            int aboveBox = (row % 2 == 0) ? col + row * w : (w - 1 - col) + row * w;
+            if (aboveBox < n) answer++;
+        }
+
+        return answer;
     }
 }
