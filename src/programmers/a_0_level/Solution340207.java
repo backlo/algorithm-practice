1215cf10cac20495f33c7c2394047ac7c6e6b858
backlo <hjun4512@gmail.com>
2025-04-28 18:39:25 +0900

    programmers 문제 풀이

---
+++ src/programmers/a_0_level/Solution340207.java
@@ -0,0 +1,10 @@
+package programmers.a_0_level;
+
+public class Solution340207 {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
