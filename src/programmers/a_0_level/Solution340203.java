122f9d2b26277dda94fcf01fc504d8a816132c35
backlo <hjun4512@gmail.com>
2025-05-04 15:31:05 +0900

    프로그래머스 문제 풀이

--- src/programmers/a_0_level/Solution340203.java
+++ src/programmers/a_0_level/Solution340203.java
@@ -1,4 +1,20 @@
 package programmers.a_0_level;
 
 public class Solution340203 {
+
+    public int[] solution(String[] cpr) {
+        int[] answer = {0, 0, 0, 0, 0};
+        String[] basic_order = {"check", "call", "pressure", "respiration", "repeat"};
+
+        for(int i=0; i<cpr.length; i++){
+            for(int j=0; j<basic_order.length; j++){
+                if(cpr[i].equals(basic_order[j])){
+                    answer[i] = j + 1;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+
 }
--- src/programmers/a_0_level/Solution340204.java
+++ src/programmers/a_0_level/Solution340204.java
@@ -1,4 +1,25 @@
 package programmers.a_0_level;
 
+import java.util.Scanner;
+
 public class Solution340204 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String code = sc.next();
+        String lastFourWords = code.substring(code.length() - 4, code.length());
+
+        if (lastFourWords.equals("_eye")) {
+            System.out.println("Ophthalmologyc");
+        } else if (lastFourWords.equals("head")) {
+            System.out.println("Neurosurgery");
+        } else if (lastFourWords.equals("infl")) {
+            System.out.println("Orthopedics");
+        } else if (lastFourWords.equals("skin")) {
+            System.out.println("Dermatology");
+        } else {
+            System.out.println("direct recommendation");
+        }
+    }
+
 }
